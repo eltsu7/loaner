2c8545904b6a7924f9c97bae0f81b6c21c82f732
Eeli Hernesniemi <eeli.hernesniemi@optofidelity>
2024-07-16 15:49:03 +0300

    test db

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use rusqlite::{Connection, Result};
+
+#[derive(Debug)]
+struct User {
+    id: i32,
+    name: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let conn = Connection::open("./test.db").unwrap();
+
+    let mut statement = conn.prepare("select * from user;").unwrap();
+    let user_iter = statement
+        .query_map([], |row| {
+            Ok(User {
+                id: row.get(0).unwrap(),
+                name: row.get(1).unwrap(),
+            })
+        })
+        .unwrap();
+
+    for user in user_iter {
+        println!(
+            "User id: {}, name: {}",
+            user.as_ref().unwrap().id,
+            user.as_ref().unwrap().name
+        );
+    }
 }
